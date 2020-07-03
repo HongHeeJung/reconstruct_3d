6176e8f39e05e1190c9c3a9c4df294eb291567c8
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-07-03 09:42:52 +0900

    H-bridge 사용하는 코드

--- stepper_motor_ino.c
+++ stepper_motor_ino.c
@@ -1,46 +1,163 @@
 
 /*
- Stepper Motor Control - speed control
+  Stepper Motor Demonstration 4
+  Stepper-Demo4.ino
+  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
 
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
- A potentiometer is connected to analog input 0.
+  DroneBot Workshop 2018
+  https://dronebotworkshop.com
+*/
 
- The motor will rotate in a clockwise direction. The higher the potentiometer value,
- the faster the motor speed. Because setSpeed() sets the delay between steps,
- you may notice the motor is less responsive to changes in the sensor value at
- low speeds.
+// Define Constants
 
- Created 30 Nov. 2009
- Modified 28 Oct 2010
- by Tom Igoe
+// Connections to A4988
+const int dirPin = 2;  // Direction
+const int stepPin = 3; // Step
 
- */
-
-#include <Stepper.h>
-
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
-
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
-int stepCount = 0;  // number of steps the motor has taken
-
+// Motor steps per rotation
+const int STEPS_PER_REV = 200;
+ 
 void setup() {
-  // nothing to do inside the setup
+  
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
 }
-
 void loop() {
-  // read the sensor value:
-  int sensorReading = analogRead(A0);
-  // map it to a range from 0 to 100:
-  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
-  // set the motor speed:
-  if (motorSpeed > 0) {
-    myStepper.setSpeed(motorSpeed);
-    // step 1/100 of a revolution:
-    myStepper.step(stepsPerRevolution / 100);
+  
+  // Set motor direction clockwise
+  digitalWrite(dirPin,HIGH); 
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(2000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(2000); 
   }
+  
+  // Pause for one second
+  delay(1000); 
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin,LOW);
+  
+  // Spin motor two rotations quickly
+  for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(1000);
+  }
+  
+  // Pause for one second
+  delay(1000);
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+/*
+  Stepper Motor Demonstration 4
+  Stepper-Demo4.ino
+  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
+ 
+  DroneBot Workshop 2018
+  https://dronebotworkshop.com
+*/
+ 
+// Define Constants
+ 
+// Connections to A4988
+const int dirPin = 2;  // Direction
+const int stepPin = 3; // Step
+ 
+// Motor steps per rotation
+const int STEPS_PER_REV = 200;
+ 
+void setup() {
+  
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+  
+  // Set motor direction clockwise
+  digitalWrite(dirPin,HIGH); 
+  
+  // Spin motor one rotation slowly
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(2000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(2000); 
+  }
+  
+  // Pause for one second
+  delay(1000); 
+  
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin,LOW);
+  
+  // Spin motor two rotations quickly
+  for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(1000);
+  }
+  
+  // Pause for one second
+  delay(1000);
 }
