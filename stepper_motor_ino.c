7289ab58e3de71085b133d3a8651949a170c4a80
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-07-08 10:45:20 +0900

    activate step motor

--- stepper_motor_ino.c
+++ stepper_motor_ino.c
@@ -1,11 +1,16 @@
-// Connections to A4988를 built-in driver version으로 수정
-const int dirPin = 2;  // Direction
-const int stepPin = 3; // Step
-const int enPin = 4; // Enable
+/*
+2020.07.03 Connections to A4988를 built-in driver version으로 수정
+2020.07.07 SBD 기준 COM이 VCC
+2020.07.08 enable이 HIGH일 때 작동
+*/
+
+const int dirPin = 2;  // Direction 회전 방향
+const int stepPin = 3; // Step  클럭을 만들어 주면 펄스 수마큼 속도가 변함
+const int enPin = 4; // Enable 1 or 연결x: WORK, 0 or GND: OFF 
 
 // Motor steps per rotation
 const int STEPS_PER_REV = 200;
-const int stepDelayMicros = 2000;
+const int stepDelayMicros = 1000;
 
 void setup() {
   
@@ -14,10 +19,11 @@ void setup() {
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
 }
+
 void loop() {
   
   digitalWrite(dirPin,HIGH); // Set motor direction clockwise
-  digitalWrite(enPin,LOW); //ENABLE 핀이 LOW일 때 드라이버가 활성화
+  digitalWrite(enPin,HIGH);
   
   // Spin motor one rotation slowly
   for(int x = 0; x < STEPS_PER_REV; x++) {
@@ -28,12 +34,14 @@ void loop() {
   }
   
   // Pause for one second
-  delay(1000); 
+  //delay(1000); //딜레이 없앰
   
-  /*
+  // clockwise로만 연속적으로 회전하도록 주석처리 함
+    /*
   // Set motor direction counterclockwise
   digitalWrite(dirPin,LOW);
-  
+  digitalWrite(enPin,HIGH);
+
   // Spin motor two rotations quickly
   for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
     digitalWrite(stepPin,HIGH);
