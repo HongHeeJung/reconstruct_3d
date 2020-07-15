2878f522941cd31f54a72e829588ae4b55768906
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-07-15 17:16:32 +0900

    ros + step motor

--- stepperCONTROL.ino
+++ stepperCONTROL.ino
@@ -1,64 +1,53 @@
 /*
- * Stepper motor control code
- * Code causes stepper to move by 1 degree everytime data is published to the "/scan" topic
- */
+2020.07.03 Connections to A4988를 built-in driver version으로 수정
+2020.07.07 SBD 기준 COM이 VCC
+2020.07.08 enable이 HIGH일 때 작동
+2020.07.15 ROS code 추가
+*/
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
-#include <Stepper.h>
+const int dirPin = 2;  // Direction 회전 방향
+const int stepPin = 3; // Step  클럭을 만들어 주면 펄스 수마큼 속도가 변함
+const int enPin = 4;   // Enable 1 or 연결x: WORK, 0 or GND: OFF 
 
-const int stepsPerRevolution = 180;  
-
-// initialize the stepper library on pins 8 through 13:
-Stepper myStepper(stepsPerRevolution, 12, 13);
-
-int pwmA = 3;
-int pwmB = 11;
-int brakeA = 9;
-int brakeB = 8;
-int dirA = 12;
-int dirB = 13;
- 
-int x = 0;
+const int STEPS_PER_REV = 180; // Motor steps per rotation
+const int stepDelayMicros = 1000;
 
 void CallBack(const std_msgs::Int16& control);
 
 ros::NodeHandle  nh;
 ros::Subscriber<std_msgs::Int16> sub("/control", &CallBack);
 
-void loop()
-{
-  nh.spinOnce();
-  delay(50);
-  printf("loop");
+void setup() {
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(enPin,OUTPUT);
+  
+  digitalWrite(enPin,HIGH);
 
-}
-
-void setup()
-{
-  Serial.begin(9600);   //initialise the serial port
-  
-  pinMode(pwmA, OUTPUT);
-  pinMode(pwmB, OUTPUT);
-  pinMode(brakeA, OUTPUT);
-  pinMode(brakeB, OUTPUT);
-  digitalWrite(pwmA, HIGH);
-  digitalWrite(pwmB, HIGH);
-  digitalWrite(brakeA, LOW);
-  digitalWrite(brakeB, LOW);
-  
-  // set the speed at 60 rpm:
-  myStepper.setSpeed(10);
-  
   nh.initNode();
   nh.subscribe(sub);
 }
 
+void loop() {
+  nh.spinOnce();
+  // Spin motor one rotation
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(stepDelayMicros); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(stepDelayMicros); 
+  }
+}
+
 void CallBack(const std_msgs::Int16& control)
 {
+    // direction of rotation
   if(control.data == 1) {
-    myStepper.step(1);
+    digitalWrite(dirPin,HIGH); // Set motor direction clockwise
   } else {
-    myStepper.step(-1);
+    digitalWrite(dirPin,LOW); // Set motor direction counterclockwise
   }
 }
