ae7b87689274531f31c7d958b8f03b7384755210
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-07-27 09:17:11 +0900

    Update and rename node_v2.cpp to node.cpp

--- rplidar_ros/src/node.cpp
+++ rplidar_ros/src/node.cpp
@@ -190,7 +190,7 @@ int main(int argc, char * argv[]) {
     std::string tcp_ip;
     std::string serial_port;
     int tcp_port = 20108;
-    int serial_baudrate = 115200;
+    int serial_baudrate = 256000;
     std::string frame_id;
     bool inverted = false;
     bool angle_compensate = true;
