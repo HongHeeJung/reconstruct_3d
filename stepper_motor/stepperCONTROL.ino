cf7e4f8dd70ed286958d9725ff4566b75cc5cb75
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-07-30 11:26:16 +0900

    Update stepperCONTROL.ino

--- stepper_motor/stepperCONTROL.ino
+++ stepper_motor/stepperCONTROL.ino
@@ -1,8 +1,11 @@
 /*
-2020.07.03 Connections to A4988를 built-in driver version으로 수정
-2020.07.07 SBD 기준 COM이 VCC
-2020.07.08 enable이 HIGH일 때 작동
-2020.07.15 ROS code 추가
+2020.07.03 Revise 'Connections to A4988' to 'built-in driver version'
+2020.07.07 COM is VCC(5V) for SBD stepper motor
+2020.07.08 if(enable == HIGH) ON
+2020.07.15 Add ROS code for ros serial comm.
+2020.07.29 Initialize motor postion by turnning the enable pin - del
+2020.07.30 Add publish code for alerting one revolution 
+           & set up one revolution for constant velocity
 */
 
 #include <ros.h>
@@ -11,46 +14,56 @@ const int dirPin = 2;  // Direction 회전 방향
 const int stepPin = 3; // Step  클럭을 만들어 주면 펄스 수마큼 속도가 변함
 const int enPin = 4;   // Enable 1 or 연결x: WORK, 0 or GND: OFF 
 
-const int STEPS_PER_REV = 240; // Motor steps per rotation (1.5 degree per step)
+const int STEPS_PER_REV = 120; // Motor steps per rotation (1.5 degree per step)
 const int stepDelayMicros = 4750;
 
 void CallBack(const std_msgs::Int16& control);
 
-ros::NodeHandle  nh;
+ros::NodeHandle nh;
 ros::Subscriber<std_msgs::Int16> sub("/control", &CallBack);
+std_msgs::Int16 msg;
+ros::Publisher pub("/one_rev", &msg);
 
 void setup() {
   // Setup the pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
-  digitalWrite(enPin,LOW);
-  //digitalWrite(enPin,HIGH); //for motor only test
-
-  nh.initNode();
-  nh.subscribe(sub);
-}
-
-void loop() {
-  nh.spinOnce();
-  // Spin motor one rotation
+  digitalWrite(enPin,High);
+  // revolve one rotation for linear revolution
   for(int x = 0; x < STEPS_PER_REV; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(stepDelayMicros); 
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(stepDelayMicros);
   }
-  digitalWrite(enPin,LOW);
+
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(pub);
+}
+
+void loop() {
+  nh.spinOnce();
+  // Spin motor one rotation
+  msg.data = 0;
+  pub.Publish(&msg);
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(stepDelayMicros); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(stepDelayMicros);
+  }
+  msg.data = 180;
+  pub.Publish(&msg);
 }
 
 void CallBack(const std_msgs::Int16& control)
 {
   // direction of rotation
   if(control.data == 0) {
-    digitalWrite(enPin,HIGH);
     digitalWrite(dirPin,HIGH); // Set motor direction clockwise
   } else {
-    digitalWrite(enPin,HIGH);
     digitalWrite(dirPin,LOW); // Set motor direction counterclockwise
   }
 }
