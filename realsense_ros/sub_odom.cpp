f048ec0ee9916ec248e065fab295bc18f5831159
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-08-12 10:31:01 +0900

    Create sub_odom.cpp

---
+++ realsense_ros/sub_odom.cpp
@@ -0,0 +1,44 @@
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ros/ros.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+double timeLaserOdometry;
+
+bool newLaserOdometry = false;
+
+float transformSum[6] = {0};
+
+void laserOdometryHandler(const nav_msgs::Odometry::ConstPtr& laserOdometry)
+{
+  timeLaserOdometry = laserOdometry->header.stamp.toSec();
+
+  double roll, pitch, yaw;
+  geometry_msgs::Quaternion geoQuat = laserOdometry->pose.pose.orientation;
+  tf::Matrix3x3(tf::Quaternion(geoQuat.z, -geoQuat.x, -geoQuat.y, geoQuat.w)).getRPY(roll, pitch, yaw);
+
+  transformSum[0] = -pitch;
+  transformSum[1] = -yaw;
+  transformSum[2] = roll;
+
+  transformSum[3] = laserOdometry->pose.pose.position.x;
+  transformSum[4] = laserOdometry->pose.pose.position.y;
+  transformSum[5] = laserOdometry->pose.pose.position.z;
+
+  newLaserOdometry = true;
+  ROS_INFO("Subscribe Odometry");
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "laserMapping");
+  ros::NodeHandle nh;
+  ros::Subscriber subLaserOdometry = nh.subscribe<nav_msgs::Odometry> 
+                                     ("/camera/odom/sample", 5, laserOdometryHandler);
+}
