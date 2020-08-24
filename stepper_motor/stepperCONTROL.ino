d43974cb79bab30764462849f6bfdc69ad145e8a
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-08-24 16:46:02 +0900

    faster version

--- stepper_motor/stepperCONTROL.ino
+++ stepper_motor/stepperCONTROL.ino
@@ -1,68 +1,78 @@
 /*
-For other stepper motor
+stepper motor control - publish direction version
 
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
+2020.07.03 Revise 'Connections to A4988' to 'built-in driver version'
+2020.07.07 COM is VCC(5V) for SBD stepper motor
+2020.07.08 if(enable == HIGH) ON
+2020.07.15 Add ROS code for ros serial comm.
+2020.07.31 Publish direction node to control the Start point
+2020.08.01 Add delay microseconds before runnung the motor
+
 */
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
-#include <Stepper.h>
+const int dirPin = 2;  // Direction 회전 방향
+const int stepPin = 3; // Step  클럭을 만들어 주면 펄스 수마큼 속도가 변함
+const int enPin = 4;   // Enable 1 or 연결x: WORK, 0 or GND: OFF 
 
-const int stepsPerRevolution = 180;  
+const int STEPS_PER_REV = 800; // Motor steps per rotation (1.5 degree per step)
+const int stepDelayMicros = 3500;
+int direction_controller = -1;
+int s = 0;
 
-// initialize the stepper library on pins 8 through 13:
-Stepper myStepper(stepsPerRevolution, 12, 13);
+void startCallback(const std_msgs::Int16& start);
 
-int pwmA = 3;
-int pwmB = 11;
-int brakeA = 9;
-int brakeB = 8;
-int dirA = 12;
-int dirB = 13;
+ros::NodeHandle node_;
+// Create Direction message
+std_msgs::Int16 Direction;
+ros::Subscriber<std_msgs::Int16> sub_start("/start", &startCallback);
+ros::Publisher pub_direction("/direction", &Direction);
+
+void setup() {
+  // Setup the pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(enPin,OUTPUT);
+  digitalWrite(enPin,LOW);
+  //digitalWrite(dirPin,LOW); //CCW
+  digitalWrite(dirPin,HIGH); //CW
+  
+  
+  Direction.data = direction_controller;
+  pub_direction.publish(&Direction);
  
-int x = 0;
-
-void CallBack(const std_msgs::Int16& control);
-
-ros::NodeHandle  nh;
-ros::Subscriber<std_msgs::Int16> sub("/control", &CallBack);
-
-void loop()
-{
-  nh.spinOnce();
-  delay(50);
-  printf("loop");
-
+  node_.initNode();
+  node_.advertise(pub_direction);
+  node_.subscribe(sub_start);
 }
 
-void setup()
-{
-  Serial.begin(9600);   //initialise the serial port
-  
-  pinMode(pwmA, OUTPUT);
-  pinMode(pwmB, OUTPUT);
-  pinMode(brakeA, OUTPUT);
-  pinMode(brakeB, OUTPUT);
-  digitalWrite(pwmA, HIGH);
-  digitalWrite(pwmB, HIGH);
-  digitalWrite(brakeA, LOW);
-  digitalWrite(brakeB, LOW);
-  
-  // set the speed at 10 rpm:
-  myStepper.setSpeed(10);
-  
-  nh.initNode();
-  nh.subscribe(sub);
-}
-
-void CallBack(const std_msgs::Int16& control)
-{
-  if(control.data == 1) {
-  // step one revolution  in one direction:
-    myStepper.step(1);
+void startCallback(const std_msgs::Int16& start){
+  s = start.data;
+  if(start.data == 0){
+    digitalWrite(enPin,LOW);
+    Serial.print("========== WAITTING... ==========");
   } else {
-  // step one revolution in the other direction:
-    myStepper.step(-1);
+    digitalWrite(enPin,HIGH);
+    Serial.print("========== START! ==========");
+  }  
+}
+
+void loop() {
+  node_.spinOnce();
+  // Spin motor one rotation
+  if(s == 1){
+    Direction.data = direction_controller;
+    pub_direction.publish(&Direction);
+    printf("========== Direction: [%d] ==========", direction_controller);
   }
+  
+  //revolve 180 deg.
+  for(int x = 0; x < STEPS_PER_REV; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(stepDelayMicros); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(stepDelayMicros);
+  }
+  direction_controller = (direction_controller+1)%2;
 }
