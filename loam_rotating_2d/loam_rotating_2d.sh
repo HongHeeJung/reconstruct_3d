10b539b4b6c69341e5b4f1db41237db0c947533f
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-08-25 13:45:02 +0900

    Update loam_rotating_2d.sh

--- loam_rotating_2d/loam_rotating_2d.sh
+++ loam_rotating_2d/loam_rotating_2d.sh
@@ -8,21 +8,25 @@ cd ~/catkin_ws_3
 catkin_make
 source ./devel/setup.bash
 
-sudo notilus rosrun rosserial_python serial_node.py /dev/ttyUSB1
+echo "Running loam_rotating_2d"
+terminal -e rosrun rosserial_python serial_node.py /dev/ttyUSB1
+echo $0
 if [ "$?" != "0" ]; then
 	echo "Cannot run Arduino" 1>&2
     popd
 	exit 1
 fi
 
-sudo notilus terminal roslaunch realsense2_camera rs_t265.launch
+terminal -e roslaunch realsense2_camera rs_t265.launch
+echo $0
 if [ "$?" != "0" ]; then
 	echo "Cannot run Real-sense camera" 1>&2
     popd
 	exit 1
 fi
 
-sudo notilus terminal roslaunch loam_rotating_2d ALL.launch
+terminal -e roslaunch loam_rotating_2d ALL.launch
+echo $0
 if [ "$?" != "0" ]; then
 	echo "Cannot launch loam_rotating_2d" 1>&2
     popd
