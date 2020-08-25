953326f5884a978bc1d8cb506676ffdf37047321
HeeJung Hong <48550633+HongHeeJung@users.noreply.github.com>
2020-08-25 21:13:06 +0900

    Update (script)stepper_motor_pubDir.ino

--- stepper_motor/(script)stepper_motor_pubDir.ino
+++ stepper_motor/(script)stepper_motor_pubDir.ino
@@ -1,82 +1,91 @@
 /*
-stepper motor control - subscribe direction + publish step version
+stepper motor control - publish direction version
 
     2020.07.03 Revise 'Connections to A4988' to 'built-in driver version'
     2020.07.07 COM is VCC(5V) for SBD stepper motor
     2020.07.08 if(enable == HIGH) ON
     2020.07.15 Add ROS code for ros serial comm.
-    2020.07.29 Initialize motor postion by turnning the enable pin - delete
-    2020.07.30 Add publish code for alerting one revolution. 
-              & set up one revolution for constant velocity.
+    2020.07.31 Publish direction node to control the Start point
+    2020.08.01 Add delay microseconds before runnung the motor
+    2020.08.24 Faster version
     2020.08.25 Add command for exit when revolution is up to 180 deg.
 */
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
 
+/*
 #include <stdio.h>
 #include <coino.h>
 #define MAX_LEN_LINE    10
+*/
 
 const int dirPin = 2;  // Direction 회전 방향
 const int stepPin = 3; // Step  클럭을 만들어 주면 펄스 수마큼 속도가 변함
 const int enPin = 4;   // Enable 1 or 연결x: WORK, 0 or GND: OFF 
 
-const int STEPS_PER_REV = 120; // Motor steps per rotation (1.5 degree per step)
-const int stepDelayMicros = 4750;
+const int STEPS_PER_REV = 800; // Motor steps per rotation (1.5 degree per step)
+const int stepDelayMicros = 3500;
+int direction_controller = -1;
+int s = 0;
 
-void CallBack(const std_msgs::Int16& control);
+void startCallback(const std_msgs::Int16& start);
 
 ros::NodeHandle node_;
-ros::Subscriber<std_msgs::Int16> sub_dir("/control", &CallBack);
-// Create StepNo message
-std_msgs::Int16 StepNo;
-ros::Publisher pub_step("/StepNo", &StepNo);
+// Create Direction message
+std_msgs::Int16 Direction;
+ros::Subscriber<std_msgs::Int16> sub_start("/start", &startCallback);
+ros::Publisher pub_direction("/direction", &Direction);
 
 void setup() {
   // Setup the pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   pinMode(enPin,OUTPUT);
-  digitalWrite(enPin,High);
-  // revolve one rotation for linear revolution
-  for(int x = 0; x < STEPS_PER_REV; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(stepDelayMicros); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(stepDelayMicros);
-  }
-
+  digitalWrite(enPin,LOW);
+  //digitalWrite(dirPin,LOW); //CCW
+  digitalWrite(dirPin,HIGH); //CW
+  
+  
+  Direction.data = direction_controller;
+  pub_direction.publish(&Direction);
+ 
   node_.initNode();
-  node_.subscribe(sub_dir);
-  node_.advertise(pub_step);
+  node_.advertise(pub_direction);
+  node_.subscribe(sub_start);
+}
+
+void startCallback(const std_msgs::Int16& start){
+  s = start.data;
+  if(start.data == 0){
+    digitalWrite(enPin,LOW);
+    Serial.print("========== WAITTING... ==========");
+  } else {
+    digitalWrite(enPin,HIGH);
+    Serial.print("========== START! ==========");
+  }  
 }
 
 void loop() {
   node_.spinOnce();
   // Spin motor one rotation
-  StepNo.data = 0;
-  pub_step.Publish(&StepNo);
+  if(s == 1){
+    Direction.data = direction_controller;
+    pub_direction.publish(&Direction);
+    printf("========== Direction: [%d] ==========", direction_controller);
+  }
+  
+  //revolve 180 deg.
   for(int x = 0; x < STEPS_PER_REV; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(stepDelayMicros); 
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(stepDelayMicros);
   }
-  StepNo.data = 120; //revolve 180 deg.
-  pub_step.Publish(&StepNo);
+  direction_controller = (direction_controller+1)%2;
 }
-
-void CallBack(const std_msgs::Int16& control)
-{
-  // direction of rotation
-  if(control.data == 0) {
-    digitalWrite(dirPin,HIGH); // Set motor direction clockwise
-  } else {
-    digitalWrite(dirPin,LOW); // Set motor direction counterclockwise
-  }
-}
-
+/*
+// 이거 안됨 ㅜㅜ
 int main(){
   char command[MAX_LEN_LINE];
   char *args[] = {command, NULL};
@@ -97,7 +106,7 @@ int main(){
       command[len - 1] = '\0'; 
     } 
 
-    /* 1. exit 입력시 shell 종료 */
+    // exit 입력시 shell 종료 
     if((!strcmp("exit",command)) && (StepNo.data == 120)){
       printf("exit!");
       return 0;
@@ -106,3 +115,4 @@ int main(){
   }
   return 0;
 }
+*/
